68d1c8dc64dd2c2947429cf8ed6a9bdd272a4abd
RostyslavDubinin <75220575+RostyslavDubinin@users.noreply.github.com>
2021-02-03 22:47:45 +0200

    11

--- js/modal.js
+++ js/modal.js
@@ -14,8 +14,11 @@
 
 
     const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobilMenuRef = document.querySelector("[data-menu]");
     menuBtnRef.addEventListener("click", () => {
       menuBtnRef.classList.toggle("is-open");
+      mobilMenuRef.classList.toggle("is-open");
+
   
     });
 })();
